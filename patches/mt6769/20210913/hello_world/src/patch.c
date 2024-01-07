f3dc715f5a3da19913037003e98c8f85439e89e7
Daniel Wegemer <daniel@wegemer.com>
2024-01-07 20:25:05 +0100

    added patch ld and changed patch.c

--- patches/mt6769/20210913/hello_world/src/patch.c
+++ patches/mt6769/20210913/hello_world/src/patch.c
@@ -9,7 +9,7 @@ void my_patch(void *a1, void *a2, void *a3, void *a4, void *a5, void *a6) {
     print_to_logshell(a1, a2, a3, a4, a5, a6);
 }
 
-__attribute__((at(0xf01c3cc6, "", CHIP_VER_MTK, FW_VER_MTK, "segment2_00409200_mod.bin")))
+__attribute__((at(0xf01c3cc6, "", CHIP_VER_MTK, FW_VER_MTK, "out_3_0xf0177000")))
 JalPatch(my_patch, my_patch);
 
 //alternative using call8 instead of overwriting literal pool table:
